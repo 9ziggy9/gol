b42ebb2ade30629db1f91e7e58387f6c2e0fc7cf
Ziggy <Zigs@HPProBook>
2021-05-15 21:28:23 -0700

    Installation script correct now.

--- install.sh
+++ install.sh
@@ -3,9 +3,10 @@
 git submodule update --init
 echo "FindSDL2.cmake submodules cloned."
 mkdir build && cd build
-cmake --build .. --target clean
-make || { echo 'Installation failed.' ; exit 1; }
-cd .. 
-ln -s build/conway conway
+cmake ..
+make install || { echo 'Installation failed.' ; exit 1; }
+echo "Cleaning up build directory."
+cd ..
+rm -rf build
 echo "Installation complete."
 echo "Run ./conway from here or add to your PATH."
